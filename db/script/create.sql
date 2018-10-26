359b9c5ef199061c0eaeb81b6798afb1d1fbc457
admin <admin@DESKTOP-UL5MSP7>
2018-10-26 15:01:28 +0800

    修改伙伴关系表：添加操作人和修改时间

--- db/script/create.sql
+++ db/script/create.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/10/25 9:11:45                           */
+/* Created on:     2018/10/26 15:00:09                          */
 /*==============================================================*/
 
 
@@ -20,7 +20,9 @@ create table PRM_PARTNER
    IS_ENABLED           bool not null default true comment '是否启用',
    SALESMAN_ID          bigint comment '业务员ID',
    REMARK               varchar(300) comment '备注',
+   OP_ID                bigint not null comment '操作人ID',
    CREATE_TIME          datetime not null comment '创建时间',
+   MODIFY_TIME          datetime not null comment '修改时间',
    primary key (ID)
 );
 
