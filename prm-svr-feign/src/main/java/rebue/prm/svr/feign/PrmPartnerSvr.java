c9653a4779a31d6a62d3a5410f9e4092bc090027
lbl <lbl@lbl>
2018-11-01 16:55:29 +0800

    添加feign子项目并添加根据id查询伙伴信息的方法

---
+++ prm-svr-feign/src/main/java/rebue/prm/svr/feign/PrmPartnerSvr.java
@@ -0,0 +1,20 @@
+package rebue.prm.svr.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import rebue.prm.mo.PrmPartnerMo;
+import rebue.sbs.feign.FeignConfig;
+
+@FeignClient(name = "prm-svr", configuration = FeignConfig.class)
+public interface PrmPartnerSvr {
+
+	/**
+	 * 获取单个伙伴信息
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@GetMapping("/prm/partner/getbyid")
+	PrmPartnerMo getById(@RequestParam("id") java.lang.Long id);
+}
