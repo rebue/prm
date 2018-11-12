cc4423bb9f351c36d634bf19d234cc14485ac81f
zbz <nnzbz@qq.com>
2018-11-12 10:23:59 +0800

    * 伙伴表添加所属组织与伙伴名称的unique * 伙伴表的ORG_ID去掉非空约束

--- db/script/create.sql
+++ db/script/create.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/11/9 10:48:49                           */
+/* Created on:     2018/11/12 10:01:45                          */
 /*==============================================================*/
 
 
@@ -15,15 +15,18 @@ create table PRM_PARTNER
    PARTNER_NAME         varchar(50) not null comment '伙伴名称',
    COMPANY_ADDRESS      varchar(120) comment '公司地址',
    CONTACT              varchar(50) not null comment '联系方式',
-   ORG_ID               bigint not null comment '组织ID',
+   ORG_ID               bigint comment '组织ID
+            当前伙伴如果是线上的，进行统一管理其组织，设置对应的组织ID，
+            如果是线下的伙伴，不用设置，可为null',
    IS_ENABLED           bool not null default true comment '是否启用',
    SALESMAN_ID          bigint comment '业务员ID',
    REMARK               varchar(300) comment '备注',
    OP_ID                bigint not null comment '操作人ID',
-   OP_ORG_ID            bigint not null comment '操作组织',
+   OP_ORG_ID            bigint not null comment '所属组织',
    CREATE_TIME          datetime not null comment '创建时间',
    MODIFY_TIME          datetime not null comment '修改时间',
-   primary key (ID)
+   primary key (ID),
+   unique key AK_OP_ORG_AND_PARTNER_NAME (PARTNER_NAME, OP_ORG_ID)
 );
 
 alter table PRM_PARTNER comment '伙伴信息';
--- db/script/update.sql
+++ db/script/update.sql
@@ -1,2 +1,14 @@
-2018年11月9日10:50:19 PRM_PARTNER添加操作组织（OP_ORG_ID）
-	alter table PRM_PARTNER add OP_ORG_ID            bigint not null comment '操作组织';
+-- 2018-11-12
+alter table PRM_PARTNER add unique key AK_OP_ORG_AND_PARTNER_NAME(PARTNER_NAME, OP_ORG_ID);
+alter table PRM_PARTNER modify ORG_ID               bigint null comment '组织ID
+            当前伙伴如果是线上的，进行统一管理其组织，设置对应的组织ID，
+            如果是线下的伙伴，不用设置，可为null';
+
+
+
+
+-- ---------------------------------------- 下面的已经更新到线上 ----------------------------------------
+
+
+-- 2018-11-9
+alter table PRM_PARTNER add OP_ORG_ID            bigint not null comment '操作组织';
