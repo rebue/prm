f9ae5ca62831bad7627754a13dee630dbcb7d076
zbz <nnzbz@qq.com>
2019-05-15 16:38:24 +0800

    * 补充注释数据库备注

--- db/script/create.sql
+++ db/script/create.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/11/12 10:01:45                          */
+/* Created on:     2019/5/15 16:35:55                           */
 /*==============================================================*/
 
 
@@ -16,13 +16,13 @@ create table PRM_PARTNER
    COMPANY_ADDRESS      varchar(120) comment '公司地址',
    CONTACT              varchar(50) not null comment '联系方式',
    ORG_ID               bigint comment '组织ID
-            当前伙伴如果是线上的，进行统一管理其组织，设置对应的组织ID，
-            如果是线下的伙伴，不用设置，可为null',
+            当前伙伴的组织ID，添加伙伴时，先在组织表中查找有没有相同名称的组织，如果有，直接关联过来；如果没有，先添加新的组织，再关联过来',
    IS_ENABLED           bool not null default true comment '是否启用',
    SALESMAN_ID          bigint comment '业务员ID',
    REMARK               varchar(300) comment '备注',
    OP_ID                bigint not null comment '操作人ID',
-   OP_ORG_ID            bigint not null comment '所属组织',
+   OP_ORG_ID            bigint not null comment '所属组织
+            添加伙伴时，取当前用户的组织ID设置进来',
    CREATE_TIME          datetime not null comment '创建时间',
    MODIFY_TIME          datetime not null comment '修改时间',
    primary key (ID),
